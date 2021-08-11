79d44c751bb13aa8481d191e9c9f9526ad4bbbcc
Fardeenmirza-019 <fardeenmirza90@yahoo.com>
2021-08-11 01:27:40 -0700

    signup remain stills
    login

--- academy_lms/test/widget_test.dart
+++ academy_lms/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:academy_lms/main.dart';
+import 'package:flutter_login_ui/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
